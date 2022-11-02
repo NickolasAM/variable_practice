c60eeece4d57a7a24cf8798157124888af68f52a
Nickolas Marquez <nickomarquez7@gmail.com>
2022-11-01 22:51:04 -0600

    Variables add

--- variables.rb
+++ variables.rb
@@ -1,7 +1,21 @@
 car = "Ferrari"
+car = "Aston Martin"
 pet = "Dog"
+pet = "Cat"
 age = 29
 fingers = 5
 amount_paid = 120.00
 deposit_paid = true
 beautiful_day = true
+
+puts "car_type"
+
+puts "animal_species"
+
+puts "student_age"
+
+puts "amount_fingers"
+
+puts amount_paid
+
+puts deposit_paid
